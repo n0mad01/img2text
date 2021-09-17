27f7e6693e2b1a56fb3d59c06c3baa12cfce5306
Adrian Soluch <adrian.soluch@gmail.com>
2021-09-17 12:27:30 +0200

    ommit tab1

--- src/app/tabs/tabs-routing.module.ts
+++ src/app/tabs/tabs-routing.module.ts
@@ -7,10 +7,10 @@ const routes: Routes = [
     path: 'tabs',
     component: TabsPage,
     children: [
-      {
+      /*{
         path: 'tab1',
         loadChildren: () => import('../tab1/tab1.module').then(m => m.Tab1PageModule)
-      },
+      },*/
       {
         path: 'tab2',
         loadChildren: () => import('../tab2/tab2.module').then(m => m.Tab2PageModule)
@@ -28,7 +28,7 @@ const routes: Routes = [
   },
   {
     path: '',
-    redirectTo: '/tabs/tab1',
+    redirectTo: '/tabs/tab2',
     pathMatch: 'full'
   }
 ];
